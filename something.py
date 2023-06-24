fda65bd7eb29b43cff0df111d3c3d6c83b55d82e
securemedjay <securemedjay@gmail.com>
2023-06-24 23:49:04 +0100

    moving this to Colab

--- something.py
+++ something.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from keras.dataset import mnist
 import matplotlib.pyplot as plt
 
@@ -5,3 +8,4 @@ import matplotlib.pyplot as plt
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 plt.subplot(221)
+plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
